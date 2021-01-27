196f1bfd28ead038e1d7414d1b8b596f393d8db7
Vlad202 <vladandum08@gmail.com>
2021-01-28 04:47:02 +0800

    fix h10 error

--- djangoProject/wsgi.py
+++ djangoProject/wsgi.py
@@ -1,7 +1,7 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ToDoDjango.settings")
+
+application = get_wsgi_application()
