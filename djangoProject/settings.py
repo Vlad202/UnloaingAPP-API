ddfec40be837659739894739f68f25d6795c3e26
Vlad202 <vladandum08@gmail.com>
2021-01-29 00:20:00 +0800

    fix settings.py rest gramework authentication classes

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -46,11 +46,12 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
 
 ROOT_URLCONF = 'djangoProject.urls'
