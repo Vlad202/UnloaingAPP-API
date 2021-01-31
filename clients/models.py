aeaca58fe13ba7587929f6f805c27eed18996bee
Vlad202 <vladandum08@gmail.com>
2021-02-01 00:33:46 +0800

    add str to admin page

--- clients/models.py
+++ clients/models.py
@@ -6,6 +6,10 @@ class Client(models.Model):
     name = models.CharField(max_length=148)
     description = models.TextField()
 
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
 class UnLoading(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     details = models.TextField(default='')
@@ -13,3 +17,6 @@ class UnLoading(models.Model):
     price = models.PositiveIntegerField()
     alredy_paid = models.PositiveIntegerField(default=0)
     workers = models.ManyToManyField(User)
+
+    def __str__(self):
+        return '{} {}, Цена: {}, Заплачено: {}'.format(self.client.name, self.date, self.price, self.alredy_paid)
